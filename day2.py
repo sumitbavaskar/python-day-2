ba697311ec2b427c75c7cbaaebc2d4af2068024c
sumit <141015819+sumitbavaskar@users.noreply.github.com>
2025-05-31 14:37:26 +0530

    .

---
+++ day2.py
@@ -0,0 +1,61 @@
+# Write a Python program that takes a number N as input and prints all even numbers from 1 to N.
+
+# n = int(input("enter the number to print even number:"))
+# i = 1
+# while i <=n:
+#     if i% 2 == 0:
+#         print(i)
+#     i+=1
+       
+# Write a Python program that takes a number N as input and prints the sum of all natural numbers from 1 to N.
+
+# n = int(input("enter the number to print sum:"))
+# sum = 0 
+# i = 1
+# while i<=n:
+#     sum+=i
+#     i+=1
+# print(sum)    
+
+# Write a Python program that takes a string as input and prints its reverse.
+
+# s=input("enter the string to reverse:")
+# print(s[::-1])
+
+# Write a Python program that takes a string as input and counts how many vowels (a, e, i, o, u) it contains.
+
+# s=input("enter the string to count vowel:")
+
+# count = 0
+# for char in s :
+#     if char.lower() in 'aeiou':
+#         count+=1
+#     elif char.upper() in 'AEIOU':
+#         count+=1
+# print(count)            
+
+
+# s = input("enter the string to count vowel:")
+
+# count = 0
+# i = 0
+# while i < len(s):
+#     if s[i].lower() in 'aeiou':
+#         count += 1
+#     i += 1
+
+# print(count)
+
+
+
+# Write a Python program to check whether the given number is a perfect square or not.
+# n = int(input("enter the number :"))
+
+# i=1
+# while i*i<=n:
+#     if i*i==n:
+#         print("perfect square number")
+#         break
+#     i+=1
+# else:
+#     print("not perfect sqaure number") 
